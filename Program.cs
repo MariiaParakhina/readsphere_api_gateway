a9325565c9ffc891e4d742203c58119c58df9ce0
MariiaParakhina <114915319+MariiaParakhina@users.noreply.github.com>
2024-04-25 18:57:02 +0200

    set up jwt validation

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Authentication.Certificate;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,7 +43,51 @@ builder.Services.AddCors((options) =>
         });
 });
 var app = builder.Build();
+app.Use(async (context, next) =>
+{
+    // Skip for paths that don't require authentication
+    if (context.Request.Path.StartsWithSegments("/api/auth/login") ||
+        context.Request.Path.StartsWithSegments("/api/Account/register"))
+    {
+        await next();
+        return;
+    }
 
+    var jwtHandler = new JwtSecurityTokenHandler();
+    var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+    // Check if token is present
+    if (string.IsNullOrEmpty(token))
+    {
+        string authorizationHeader = context.Request.Headers["Authorization"].ToString();
+ 
+        byte[] byteArray = Encoding.UTF8.GetBytes(authorizationHeader);
+ 
+        Stream stream = new MemoryStream(byteArray);
+        context.Response.StatusCode = 401; // Unauthorized
+        context.Response.Body = stream;
+        return;
+    }
+
+    try
+    {
+        // Validate the token
+        var jwtToken = jwtHandler.ReadJwtToken(token);
+        var userIdClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "unique_name");
+
+        var userId = userIdClaim.Value;
+        context.Request.Headers.Add("X-User-Id", userId); 
+         
+    }
+    catch (Exception ex)
+    {
+        context.Response.StatusCode = 401;  
+        await context.Response.WriteAsync(ex.Message);
+        return;
+    }
+
+    await next();
+});
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
