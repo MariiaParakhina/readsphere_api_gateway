7df348dfa531fbeb17099580f15e1e588ae6eb04
MariiaParakhina <114915319+MariiaParakhina@users.noreply.github.com>
2024-05-26 15:57:14 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,23 +6,10 @@ using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-/*builder.WebHost.ConfigureKestrel((context, options) =>
-{
-    var certPath = "./cert/certificate.pfx";
-    options.ListenAnyIP(64212, listenOptions =>
-    {
-        listenOptions.UseHttps(certPath, "pass");
-       
-    });
-});*/
-
-// Configure certificate authentication
 builder.Services.AddAuthentication(CertificateAuthenticationDefaults.AuthenticationScheme)
     .AddCertificate(options =>
     {
@@ -36,26 +23,24 @@ builder.Services.AddAuthentication(CertificateAuthenticationDefaults.Authenticat
     });
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowLocalhost3000", corsPolicyBuilder =>
+    options.AddPolicy("AllowSpecificOrigin", builder =>
     {
-        corsPolicyBuilder.WithOrigins("http://localhost:3000")
-            .WithHeaders("Authorization", "Content-Type")
+        builder.WithOrigins("http://localhost:3000")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
     });
 });
-
 var app = builder.Build();
 
 app.Use(async (context, next) =>
 {
     if (context.Request.Method == "OPTIONS")
     {
-        context.Response.Headers.Add("Access-Control-Allow-Origin", new[] { (string)context.Request.Headers["Origin"] });
-        context.Response.Headers.Add("Access-Control-Allow-Headers", new[] { "Origin, X-Requested-With, Content-Type, Accept, Authorization" });
-        context.Response.Headers.Add("Access-Control-Allow-Methods", new[] { "GET, POST, PUT, DELETE, OPTIONS" });
-        context.Response.Headers.Add("Access-Control-Allow-Credentials", new[] { "true" });
+        context.Response.Headers.Append("Access-Control-Allow-Origin", (string)context.Request.Headers["Origin"]);
+        context.Response.Headers.Append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+        context.Response.Headers.Append("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        context.Response.Headers.Append("Access-Control-Allow-Credentials", "true");
         context.Response.StatusCode = 200;
         return;
     }
@@ -64,7 +49,6 @@ app.Use(async (context, next) =>
 });
 app.Use(async (context, next) =>
 {
-    // Skip for paths that don't require authentication
     if (context.Request.Path.StartsWithSegments("/api/auth/login") ||
         context.Request.Path.StartsWithSegments("/api/Account/register"))
     {
@@ -74,16 +58,9 @@ app.Use(async (context, next) =>
 
     var jwtHandler = new JwtSecurityTokenHandler();
     var token = context.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
-     Console.WriteLine($"{context.Request.Headers["Authorization"].IsNullOrEmpty()}");
-     Console.WriteLine($"{context.Request.Headers["Authorization"]}");
     
-     
-    
-    Console.WriteLine($"Token: {token}" );
-    // Check if token is present
     if (string.IsNullOrEmpty(token))
     {
-        
         string authorizationHeader = context.Request.Headers["Authorization"].ToString();
 
         byte[] byteArray = Encoding.UTF8.GetBytes(authorizationHeader);
@@ -96,7 +73,6 @@ app.Use(async (context, next) =>
 
     try
     {
-        // Validate the token
         var jwtToken = jwtHandler.ReadJwtToken(token);
         
         var userIdClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "nameid");
@@ -113,17 +89,15 @@ app.Use(async (context, next) =>
 
     await next();
 });
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors("AllowLocalhost3000");
+app.UseCors("AllowSpecificOrigin");
 app.UseAuthentication();
 app.UseAuthorization();
-// app.UseHttpsRedirection();  
-// app.UseHsts(); 
 app.MapReverseProxy();
 
 app.Run();
